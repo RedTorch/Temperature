5d62cd2181baef8e64cd5cfb2e358d0a5da57df1
DavidYeLuo <dayeluo@galileoweb.org>
2016-10-22 12:23:49 -0700

    Passed all tests

--- Temperature.java
+++ Temperature.java
@@ -15,7 +15,7 @@ public class Temperature{
 	{
 		degrees = dDegrees;
 		type = 'C';
-        scanner = new Scanner(System.in);
+                scanner = new Scanner(System.in);
 	}
 	public Temperature(char cType)   //defaults to 0.0 degrees
 	{
@@ -27,42 +27,55 @@ public class Temperature{
 	{
 		type = 'C';
 		degrees = 0.0;
-        scanner = new Scanner(System.in);
+                scanner = new Scanner(System.in);
 	}
 
 	public void writeOutput()   //prints temp(C), temp(F)
 	{
-		System.out.println("<" + Math.round(getC()*10)/10.0 + "> degrees Celsius, <" + Math.round(getF()*10)/10.0 + "> degrees Fahrenheit");
-	}	
+		System.out.println("<" + getC() + "> degrees Celsius, <" + getF() + "> degrees Fahrenheit");
+	}
 	public void writeC()   //prints temp(C)
 	{
-		System.out.println("<" + Math.round(getC()*10)/10.0 + "> degrees Celsius");
+		System.out.println("<" + getC() + "> degrees Celsius");
 	}
 	public void writeF()   //prints temp(F)
 	{
-		System.out.println("<" + Math.round(getF()*10)/10.0 + "> degrees Fahrenheit");
+		System.out.println("<" + getF() + "> degrees Fahrenheit");
 	}
 
 	public double getC()   //Celsius
 	{
-		if(type == 'F')
+		
+		double conversion = 10.0;	// To see 1 decimal after a number
+		if(type == 'F' || type == 'f')  // change from F to C
 		{
-			return (Math.round(((degrees * (9/5)) + 32)*10)/10.0);
+			// formula F = (C-32) * 5/9
+			// Subtracting 32 from C is the key
+			double firstStep = degrees - 32.0;
+			double secondStep = 5.0/9.0;
+			double answer = (double) Math.round(firstStep * secondStep * conversion) / conversion;
+			
+			return answer;
 		}
-		else
+		else // no change needed
 		{
-			return Math.round(degrees*10)/10.0;
+			return Math.round(degrees*conversion)/conversion;
 		}
 	}
-	public double getF()   //fahrenheit
+	public double getF()   //Fahrenheit
 	{
-		if(type == 'C')
+		double conversion = 10.0;
+		if(type == 'C' || type == 'c')
 		{
-			return (Math.round(((degrees * (9/5)) + 32)*10)/10.0);
+			// formula C = (F * 9/5) + 32
+			double add = 32.0;
+			double multiply = degrees * 9.0/5.0;
+			double answer = (double) Math.round((multiply + add) * conversion) / conversion;
+			return answer;
 		}
 		else
 		{
-			return Math.round(degrees*10)/10.0;
+			return Math.round(degrees*conversion)/conversion;
 		}
 	}
 	public void set(double dDegrees, char cType)
@@ -76,46 +89,38 @@ public class Temperature{
 	}
 	public void set(char cType)
 	{
-		if(type != cType && (cType == 'C' || cType == 'F'))
-		{
-			// if(type == 'C')
-			// {
-			// 	degrees = (degrees-32) * (5/9);
-			// }
-			// else
-			// {
-			// 	degrees = (degrees * (9/5)) + 32;
-			// }
-			type = cType;
-		}
+		type = cType;
 	}
 
-        public boolean equals(Temperature otherTemp) //WORK ON LATER
-	{
-		if(otherTemp.getC() == this.getC()){
+    public boolean equals(Temperature otherTemp) //WORK ON LATER
+    {
+    	// Doesn't matter which one you compare 
+    	// getC or getF will give you the same result
+		if(otherTemp.getC() == getC() && otherTemp.getF() == getF())  
+		{
 			return true;
 		}
 		else
 		{
 			return false;
 		}
-	}
+    }
 
 	public String toString()   //returns type & value; Math.round(temperature*10)/10.0
 	{
-		String toReturn = "ERROR! Invalid value of TYPE variable (not C or F). Called from the toString() method";
+		String toReturn = "ERROR! Invalid value of TYPE variable (not C or F). Called in toString";
 		if(type == 'C')
 		{
-			toReturn = Math.round(getC()*10)/10.0 + " C";
+			toReturn = getC() + " C";
 		}
 		else if(type == 'F')
 		{
-			toReturn = Math.round(getF()*10)/10.0 + " F";
+			toReturn = getF() + " F";
 		}
 		return toReturn;
 	}
 	public void readInput()
-        { 
+    { 
         // :important:
         // I am using nextLine() to get String.
         // This is because nextDouble skips nextLine for some reason
